d90e7c63228e0ee44655ef680b548fb07d228cb5
FreeCurious <50730017+FreeCurious@users.noreply.github.com>
2019-06-14 10:29:17 +0200

    Design Change
    Design change

--- UI/js/expandinggrid.js
+++ UI/js/expandinggrid.js
@@ -5,5 +5,5 @@ function openTab(tabName)
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName).style.display = "grid";
 }
